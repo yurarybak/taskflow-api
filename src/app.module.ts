c0b603472609c65ed4f47f7765e73b0e349a5e29
yurarybak <yurarybak1993@gmail.com>
2026-05-25 12:24:39 +0300

    feat: add Users module and service with initial tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
