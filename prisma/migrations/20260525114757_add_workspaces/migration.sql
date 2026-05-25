d74d9b1d6bee83409ba7faa33acf47ebd89658d0
yurarybak <yurarybak1993@gmail.com>
2026-05-25 14:48:32 +0300

    feat: add Workspaces model and migration for workspace management

---
+++ prisma/migrations/20260525114757_add_workspaces/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "workspaces" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "workspaces_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "workspaces" ADD CONSTRAINT "workspaces_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
