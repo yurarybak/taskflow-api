92e213f55f46dcd1fd06a9d2dcfb19759049066d
yurarybak <yurarybak1993@gmail.com>
2026-05-25 12:29:35 +0300

    feat: implement Auth module with controller and service, including tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    UsersModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    UsersModule,
+    AuthModule
+  ]
 })
 export class AppModule {}
