ff2df194795f5afaf80355a279a3b5a6745e5610
yurarybak <yurarybak1993@gmail.com>
2026-05-26 14:37:53 +0300

    feat: refactor create method in WorkspacesService to use transaction for workspace and member creation

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -2,17 +2,30 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
+import { WorkspaceRole } from '../../generated/prisma/enums';
 
 @Injectable()
 export class WorkspacesService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(ownerId: string, createWorkspaceDto: CreateWorkspaceDto) {
-    return this.prisma.workspace.create({
-      data: {
-        ...createWorkspaceDto,
-        ownerId,
-      },
+    return this.prisma.$transaction(async (tx) => {
+      const workspace = await tx.workspace.create({
+        data: {
+          ...createWorkspaceDto,
+          ownerId,
+        },
+      });
+
+      await tx.workspaceMember.create({
+        data: {
+          workspaceId: workspace.id,
+          userId: ownerId,
+          role: WorkspaceRole.OWNER,
+        },
+      });
+
+      return workspace;
     });
   }
 
