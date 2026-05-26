8d1f36b3fb14acc395920c013f857283ec361f18
yurarybak <yurarybak1993@gmail.com>
2026-05-26 10:42:17 +0300

    feat: add Swagger integration for API documentation in main.ts and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +15,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('TaskFlow API')
+    .setDescription('TaskFlow backend API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
