8739a8ac2356672bfbbb61d461249df37e4c8c8e
yurarybak <yurarybak1993@gmail.com>
2026-05-28 15:47:33 +0300

    feat: add assigneeId to Task model and update User model for task assignment

---
+++ prisma/migrations/20260528124704_add_task_assignee/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "tasks" ADD COLUMN     "assigneeId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
