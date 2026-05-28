be771f81954debe7732cc06c6cbcaf55b901626e
yurarybak <yurarybak1993@gmail.com>
2026-05-28 15:30:28 +0300

    feat: add ApiProperty decorators to PaginatedProjectsResponseDto for Swagger documentation

--- src/projects/dto/responses/paginated-projects-response.dto.ts
+++ src/projects/dto/responses/paginated-projects-response.dto.ts
@@ -1,7 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { ProjectResponseDto } from './project-response.dto';
 import { PaginationMetaResponseDto } from '../../../common/dto/responses/pagination-meta-response.dto';
 
 export class PaginatedProjectsResponseDto {
+  @ApiProperty({
+    type: [ProjectResponseDto],
+  })
   data!: ProjectResponseDto[];
+
+  @ApiProperty({
+    type: PaginationMetaResponseDto,
+  })
   meta!: PaginationMetaResponseDto;
 }
