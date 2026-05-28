b07912cdafe9fb269c2ca6ce347f76d23fa614b3
yurarybak <yurarybak1993@gmail.com>
2026-05-28 17:22:13 +0300

    feat: add TaskComment model and migration for task comments, update User and Task models to include comments relation

---
+++ prisma/migrations/20260528142146_add_task_comments/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "task_comments" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "task_comments_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "task_comments" ADD CONSTRAINT "task_comments_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "task_comments" ADD CONSTRAINT "task_comments_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
