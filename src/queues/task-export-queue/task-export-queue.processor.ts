c2e17dc69200ba42f5fbe2bf3a01e69bc69aa212
yurarybak <yurarybak1993@gmail.com>
2026-06-17 10:24:29 +0300

    feat: add concurrency configuration to task export queue processor

--- src/queues/task-export-queue/task-export-queue.processor.ts
+++ src/queues/task-export-queue/task-export-queue.processor.ts
@@ -11,7 +11,9 @@ import {
   TASK_EXPORT_JOBS,
 } from './task-export-queue.constants';
 
-@Processor(TASK_EXPORT_QUEUE)
+@Processor(TASK_EXPORT_QUEUE, {
+  concurrency: 2,
+})
 @Injectable()
 export class TaskExportQueueProcessor extends WorkerHost {
   constructor(private readonly taskExportsService: TaskExportsService) {
